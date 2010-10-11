ff96e429d04a82dcc42019c493b827b4d385b08c
patrick <patrick@localhost>
2010-10-11 13:32:36 +0200

    New Unittest fixture for timeelement.

---
+++ NTimeline.Test/Core/TimeElementTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using NTimeline.Core;
+
+using NUnit.Framework;
+
+namespace NTimeline.Test.Core
+{
+	[TestFixture]
+	public class TimeElementTest
+	{
+		[Test]
+		public void TestTimeElement()
+		{
+			// Arrange
+			DateTime dtDate = DateTime.Now;
+
+			// Act
+			TimeElement element = new TimeElement(dtDate, true);
+
+			// Assert
+			Assert.AreEqual(dtDate, element.Date);
+			Assert.IsTrue(element.IsFrom);
+			Assert.IsFalse(element.IsUntil);
+		}
+	}
+}
