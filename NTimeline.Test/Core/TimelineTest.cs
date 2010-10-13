4259fb072c43918f4f3355a5205f6ba57e3d29d7
patrick <patrick@localhost>
2010-10-13 11:33:00 +0200

    Timeline unit tests added

---
+++ NTimeline.Test/Core/TimelineTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using Moq;
+
+using NTimeline.Core;
+using NTimeline.Generator;
+
+using NUnit.Framework;
+
+namespace NTimeline.Test.Core
+{
+	[TestFixture]
+	public class TimelineTest
+	{
+		[Test]
+		public void TestTimeline()
+		{
+			// Arrange
+			var generator = new Mock<ITimelineGenerator>();
+
+			// Act
+			Timeline timeline = new Timeline(generator.Object);
+
+			// Assert
+			Assert.AreEqual(generator.Object, timeline.TimelineGenerator);
+		}
+
+		[Test]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void TestTimeline_With_Null_As_Parameter()
+		{
+			// Act
+			new Timeline(null);
+		}
+	}
+}
